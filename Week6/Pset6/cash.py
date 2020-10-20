d08776543cef0a407cc1a0d40996118c9a96b70d
Volkan Sahin <vsahin0985@gmail.com>
2020-10-20 20:11:56 +0300

    Update cash.py

--- Week6/Pset6/cash.py
+++ Week6/Pset6/cash.py
@@ -9,17 +9,23 @@ number = number * 100
 counter = 0
 remainder = 0
 
-if number / 25 > 1:
+if number / 25 >= 1:
     counter = counter + int(number / 25)
-    remainder = number % 25
+    remainder_25 = number % 25
+else:
+    remainder_25 = number
 
-if remainder / 10 > 1:
-    counter = counter + int(remainder / 10)
-    remainder = remainder % 10
+if remainder_25 / 10 >= 1:
+    counter = counter + int(remainder_25 / 10)
+    remainder_10 = remainder_25 % 10
+else:
+    remainder_10 = remainder_25
 
-if remainder / 5 > 1:
-    counter = counter + int(remainder / 5)
-    remainder = remainder % 5
+if remainder_10 / 5 >= 1:
+    counter = counter + int(remainder_10 / 5)
+    remainder_5 = remainder_10 % 5
+else:
+    remainder_5 = remainder_10
 
-counter = counter + remainder
+counter = counter + remainder_5
 print(int(counter))
