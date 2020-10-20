2fe53d7a2df90b5d7636169ede6d6d61143e7ff0
Volkan Sahin <vsahin0985@gmail.com>
2020-10-20 16:28:17 +0300

    Create marioless.py

---
+++ Week6/Pset6/marioless.py
@@ -0,0 +1,9 @@
+from cs50 import get_int
+
+height = get_int("Height: ")
+
+while height > 8 or height <= 0:
+    height = get_int("Height: ")
+
+for i in range(height):
+    print(" " * (height - 1 - i) + "#" * (i+1))
